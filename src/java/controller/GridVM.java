bf8c3ddabcacd2b3f86331f5a02caa7711e3b895
Marco Damaceno <maadamaceno@gmail.com>
2015-11-29 17:08:34 -0200

    Exclusão de usuários

--- src/java/controller/GridVM.java
+++ src/java/controller/GridVM.java
@@ -8,7 +8,6 @@ package controller;
 import dao.GridJpaController;
 import dao.GridPresenterJpaController;
 import dao.PresenterJpaController;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
--- src/java/controller/UserVM.java
+++ src/java/controller/UserVM.java
@@ -7,14 +7,13 @@ package controller;
 
 import dao.UserJpaController;
 import java.util.List;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import model.User;
-import org.zkoss.bind.annotation.AfterCompose;
-import org.zkoss.bind.annotation.ContextParam;
-import org.zkoss.bind.annotation.ContextType;
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zhtml.Messagebox;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
 
 /**
  *
@@ -22,13 +21,66 @@ import org.zkoss.zk.ui.select.Selectors;
  */
 public class UserVM extends BaseController
 {
-    private List<User> listUsers;
 
-    @AfterCompose
-    public void init(@ContextParam(ContextType.VIEW) Component view) {
-        Selectors.wireComponents(view, this, false);
-        
+    private List<User> listUsers;
+    private String name, email, address, phone_number, doc;
+    private boolean admin;
+    private String userId;
+    private User user;
+    private String selectedAdmin;
+
+    private User userSelected = null;
+
+    @Init
+    public void init()
+    {
         listUsers = new UserJpaController(emf).findUserEntities();
+    }
+
+    @Command
+    public void goToUserCreate()
+    {
+        Executions.sendRedirect("create.zul");
+    }
+
+    @Command
+    public void saveUser()
+    {
+        try {
+            if (userId == null) {
+                user = new User();
+                user.setName(name);
+                user.setEmail(email);
+                user.setAddress(address);
+                user.setPhoneNumber(phone_number);
+                user.setDoc(doc);
+                user.setAdmin(Boolean.parseBoolean(selectedAdmin));
+
+                new UserJpaController(emf).create(user);
+
+                messageOk("Usuário registrado com sucesso!");
+            } else {
+
+            }
+        } catch (Exception ex) {
+            Messagebox.show(ex.getMessage(), "", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+    @Command
+    public void destroyUser()
+    {
+        if (userSelected != null) {
+            Messagebox.show("Deseja excluir este usuário?",
+                    "Deseja excluir este usuário?", Messagebox.YES | Messagebox.NO, Messagebox.QUESTION, (Event t) -> {
+                        if (t.getName().equals("onYes")) {
+
+                            new UserJpaController(emf).destroy(userSelected.getId());
+
+                            messageOk("Usuário excluído com sucesso!");
+                        }
+                    });
+        }
     }
 
     public List<User> getListUsers()
@@ -40,6 +92,111 @@ public class UserVM extends BaseController
     {
         this.listUsers = listUsers;
     }
-    
-    
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getPhone_number()
+    {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number)
+    {
+        this.phone_number = phone_number;
+    }
+
+    public String getDoc()
+    {
+        return doc;
+    }
+
+    public void setDoc(String doc)
+    {
+        this.doc = doc;
+    }
+
+    @NotifyChange
+    public boolean isAdmin()
+    {
+        return admin;
+    }
+
+    @NotifyChange
+    public void setAdmin(boolean admin)
+    {
+        if (selectedAdmin.equals("true")) {
+            this.admin = true;
+        }
+        this.admin = false;
+    }
+
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    @NotifyChange
+    public String getSelectedAdmin()
+    {
+        return selectedAdmin;
+    }
+
+    @NotifyChange
+    public void setSelectedAdmin(String selectedAdmin)
+    {
+        this.selectedAdmin = selectedAdmin;
+    }
+
+    public User getUserSelected()
+    {
+        return userSelected;
+    }
+
+    public void setUserSelected(User userSelected)
+    {
+        this.userSelected = userSelected;
+    }
 }
